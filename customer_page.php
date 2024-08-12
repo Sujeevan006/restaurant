34234591b31110396f38f7e73efaacc855df2a7e
Sujeevan <avsinfo0824@gmail.com>
2024-08-12 20:40:24 +0530

    customer page modification

--- customer_page.php
+++ customer_page.php
@@ -6,11 +6,12 @@ session_start();
 
 $user_id = $_SESSION['user_id'];
 
-if(!isset($user_id)){
+if (!isset($user_id)) {
    header('location:login.php');
-};
+}
+;
 
-if(isset($_POST['add_to_wishlist'])){
+if (isset($_POST['add_to_wishlist'])) {
 
    $pid = $_POST['pid'];
    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
@@ -27,11 +28,11 @@ if(isset($_POST['add_to_wishlist'])){
    $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
    $check_cart_numbers->execute([$p_name, $user_id]);
 
-   if($check_wishlist_numbers->rowCount() > 0){
+   if ($check_wishlist_numbers->rowCount() > 0) {
       $message[] = 'already added to wishlist!';
-   }elseif($check_cart_numbers->rowCount() > 0){
+   } elseif ($check_cart_numbers->rowCount() > 0) {
       $message[] = 'already added to cart!';
-   }else{
+   } else {
       $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
       $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
       $message[] = 'added to wishlist!';
@@ -39,7 +40,7 @@ if(isset($_POST['add_to_wishlist'])){
 
 }
 
-if(isset($_POST['add_to_cart'])){
+if (isset($_POST['add_to_cart'])) {
 
    $pid = $_POST['pid'];
    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
@@ -55,14 +56,14 @@ if(isset($_POST['add_to_cart'])){
    $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
    $check_cart_numbers->execute([$p_name, $user_id]);
 
-   if($check_cart_numbers->rowCount() > 0){
+   if ($check_cart_numbers->rowCount() > 0) {
       $message[] = 'already added to cart!';
-   }else{
+   } else {
 
       $check_wishlist_numbers = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
       $check_wishlist_numbers->execute([$p_name, $user_id]);
 
-      if($check_wishlist_numbers->rowCount() > 0){
+      if ($check_wishlist_numbers->rowCount() > 0) {
          $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
          $delete_wishlist->execute([$p_name, $user_id]);
       }
@@ -78,6 +79,7 @@ if(isset($_POST['add_to_cart'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -91,136 +93,138 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php include 'header.php'; ?>
 
-<div class="home-bg">
+   <?php include 'header.php'; ?>
 
-   <section class="home">
+   <div class="home-bg">
 
-      <div class="content">
-         <h3>ABC Restaurant</h3>
-         <p>Welcome to ABC Restaurant, your destination for delicious meals and a warm atmosphere. Explore our menu, make a reservation, or order for delivery-experience the best with us</p>
-         <a href="about.php" class="btn">about us</a>
+      <section class="home">
+
+         <div class="content">
+            <h3>ABC Restaurant</h3>
+            <p>Welcome to ABC Restaurant, your destination for delicious meals and a warm atmosphere. Explore our menu,
+               make a reservation, or order for delivery-experience the best with us</p>
+            <a href="about.php" class="btn">about us</a>
+         </div>
+
+      </section>
+
+   </div>
+
+   <section class="home-category">
+
+      <div class="optionssection">
+         <div class="options">
+            <div class="optionsbox">
+               <img src="images/dining.jpg" alt="">
+               <a href="reservation.php?category=fruits" class="btn">Make a Reservation</a>
+            </div>
+
+            <div class="optionsbox">
+               <img src="images/order.jpg" alt="">
+               <a href="category.php?category=meat" class="btn">Order Foods</a>
+            </div>
+
+            <div class="optionsbox">
+               <img src="images/foodmenus.jpg" alt="">
+               <a href="category.php?category=meat" class="btn">Explore Menu</a>
+            </div>
+
+         </div>
       </div>
 
-   </section>
+      <div class="box-container">
 
-</div>
-
-<section class="home-category">
-
-<div class="optionssection">
-      <div class="options">
-         <div class="optionsbox">
+         <div class="box">
             <img src="images/dining.jpg" alt="">
             <a href="category.php?category=fruits" class="btn">Make a Reservation</a>
          </div>
 
-         <div class="optionsbox">
+         <div class="box">
             <img src="images/order.jpg" alt="">
             <a href="category.php?category=meat" class="btn">Order Foods</a>
          </div>
-         
-         <div class="optionsbox">
+
+         <div class="box">
             <img src="images/foodmenus.jpg" alt="">
             <a href="category.php?category=meat" class="btn">Explore Menu</a>
          </div>
 
       </div>
-</div>
 
-   <div class="box-container">
+      <h1 class="title">shop by category</h1>
 
-      <div class="box">
-         <img src="images/dining.jpg" alt="">
-         <a href="category.php?category=fruits" class="btn">Make a Reservation</a>
+      <div class="box-container">
+
+         <div class="box">
+            <img src="images/cat-1.png" alt="">
+            <h3>fruits</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+            <a href="category.php?category=fruits" class="btn">fruits</a>
+         </div>
+
+         <div class="box">
+            <img src="images/cat-2.png" alt="">
+            <h3>meat</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+            <a href="category.php?category=meat" class="btn">meat</a>
+         </div>
+
+         <div class="box">
+            <img src="images/cat-3.png" alt="">
+            <h3>vegitables</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+            <a href="category.php?category=vegitables" class="btn">vegitables</a>
+         </div>
+
+         <div class="box">
+            <img src="images/cat-4.png" alt="">
+            <h3>fish</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
+            <a href="category.php?category=fish" class="btn">fish</a>
+         </div>
+
       </div>
 
-      <div class="box">
-         <img src="images/order.jpg" alt="">
-         <a href="category.php?category=meat" class="btn">Order Foods</a>
-      </div>
-      
-      <div class="box">
-         <img src="images/foodmenus.jpg" alt="">
-         <a href="category.php?category=meat" class="btn">Explore Menu</a>
-      </div>
+   </section>
 
-   </div>
+   <section class="products">
 
-   <h1 class="title">shop by category</h1>
+      <h1 class="title">latest products</h1>
 
-   <div class="box-container">
+      <div class="box-container">
 
-      <div class="box">
-         <img src="images/cat-1.png" alt="">
-         <h3>fruits</h3>
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
-         <a href="category.php?category=fruits" class="btn">fruits</a>
-      </div>
+         <?php
+         $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+         $select_products->execute();
+         if ($select_products->rowCount() > 0) {
+            while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
+               ?>
+               <form action="" class="box" method="POST">
+                  <div class="price">$<span><?= $fetch_products['price']; ?></span>/-</div>
+                  <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+                  <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+                  <div class="name"><?= $fetch_products['name']; ?></div>
+                  <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+                  <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+                  <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+                  <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+                  <input type="number" min="1" value="1" name="p_qty" class="qty">
+                  <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
+                  <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+               </form>
+               <?php
+            }
+         } else {
+            echo '<p class="empty">no products added yet!</p>';
+         }
+         ?>
 
-      <div class="box">
-         <img src="images/cat-2.png" alt="">
-         <h3>meat</h3>
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
-         <a href="category.php?category=meat" class="btn">meat</a>
-      </div>
-
-      <div class="box">
-         <img src="images/cat-3.png" alt="">
-         <h3>vegitables</h3>
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
-         <a href="category.php?category=vegitables" class="btn">vegitables</a>
-      </div>
-
-      <div class="box">
-         <img src="images/cat-4.png" alt="">
-         <h3>fish</h3>
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat.</p>
-         <a href="category.php?category=fish" class="btn">fish</a>
       </div>
 
-   </div>
-
-</section>
-
-<section class="products">
-
-   <h1 class="title">latest products</h1>
-
-   <div class="box-container">
-
-   <?php
-      $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
-      $select_products->execute();
-      if($select_products->rowCount() > 0){
-         while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){ 
-   ?>
-   <form action="" class="box" method="POST">
-      <div class="price">$<span><?= $fetch_products['price']; ?></span>/-</div>
-      <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
-      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
-      <div class="name"><?= $fetch_products['name']; ?></div>
-      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-      <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
-      <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
-      <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
-      <input type="number" min="1" value="1" name="p_qty" class="qty">
-      <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
-      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
-   </form>
-   <?php
-      }
-   }else{
-      echo '<p class="empty">no products added yet!</p>';
-   }
-   ?>
-
-   </div>
-
-</section>
+   </section>
 
 
 
@@ -228,9 +232,10 @@ if(isset($_POST['add_to_cart'])){
 
 
 
-<?php include 'footer.php'; ?>
+   <?php include 'footer.php'; ?>
 
-<script src="js/script.js"></script>
+   <script src="js/script.js"></script>
 
 </body>
+
 </html>

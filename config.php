e049805ee99ff74ff69d26139e4ab24e1382a330
Sujeevan <avsinfo0824@gmail.com>
2024-09-05 20:59:01 +0530

    singleton design pattern added for config file

--- config.php
+++ config.php
@@ -1,9 +1,35 @@
 <?php
 
-$db_name = "mysql:host=localhost;dbname=restaurant";
-$username = "root";
-$password = "";
+class Database {
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $dbName = 'restaurant';
+    private $username = 'root';
+    private $password = '';
+    private function __construct() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+?>
 
-$conn = new PDO($db_name, $username, $password);
 
+<?php
+include_once 'config.php';
+$db = Database::getInstance();
+$conn = $db->getConnection();
 ?>

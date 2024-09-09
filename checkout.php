55dda8857b74369070a4a04b55ddbab3c50f34d1
Sujeevan <avsinfo0824@gmail.com>
2024-09-09 23:28:38 +0530

    place order modifications

--- checkout.php
+++ checkout.php
@@ -132,23 +132,23 @@ if(isset($_POST['order'])){
 
          <div class="inputBox">
             <span>address line 01 :</span>
-            <input type="text" name="flat" placeholder="e.g. flat number" class="box" required>
+            <input type="text" name="flat" placeholder="e.g. Number" class="box" required>
          </div>
          <div class="inputBox">
             <span>address line 02 :</span>
-            <input type="text" name="street" placeholder="e.g. street name" class="box" required>
+            <input type="text" name="street" placeholder="e.g. Street name" class="box" required>
          </div>
          <div class="inputBox">
             <span>city :</span>
-            <input type="text" name="city" placeholder="e.g. mumbai" class="box" required>
+            <input type="text" name="city" placeholder="e.g. Jaffna" class="box" required>
          </div>
          <div class="inputBox">
             <span>state :</span>
-            <input type="text" name="state" placeholder="e.g. maharashtra" class="box" required>
+            <input type="text" name="state" placeholder="e.g. Town" class="box" required>
          </div>
          <div class="inputBox">
             <span>country :</span>
-            <input type="text" name="country" placeholder="e.g. India" class="box" required>
+            <input type="text" name="country" placeholder="e.g. Sri Lanka" class="box" required>
          </div>
          <div class="inputBox">
             <span>pin code :</span>
